161bfac01c7f979a8d337565e67ed977ed08e59d
ELISHA1994 <elishabello2014@gmail.com>
2022-01-26 13:14:18 +0200

    Creating Application Models and Schemas

---
+++ backend/models/category-model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 32,
+        },
+        slug: {
+            type: String,
+            unique: true,
+            index: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model('Category', CategorySchema);
