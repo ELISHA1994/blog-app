a1b9466a49411813a9581afee212814ce897f28c
ELISHA1994 <elishabello2014@gmail.com>
2022-01-26 09:18:15 +0200

    Setting up the server

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import http from "http";
+import path from "path";
+import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import {default as logger} from "morgan";
+import {default as rfs} from "rotating-file-stream";
+import { default as DBG } from 'debug';
+import { approotdir } from "./approotdir.js";
+import {normalizePort, onError, onListening} from "./utils/utils";
+
+
+// Global variables
+const __dirname = approotdir;
+const debug = DBG('server:debug');
+const dbgerror = DBG('server:error');
+
+dotenv.config();
+
+// Initialize the express app object
+export const app = express();
+
+// Setting api port
+export const port = normalizePort(process.env.PORT || '1337');
+app.set('port', port);
+
+// Middleware
+app.use(express.json());
+app.use(logger(process.env.REQUEST_LOG_FORMAT || 'common',  {
+    stream: process.env.REQUEST_LOG_FILE ?
+        rfs.createStream(process.env.REQUEST_LOG_FILE, {
+            size: '10M',     // rotate every 10 MegaBytes written
+            interval: '1d',  // rotate daily
+            compress: 'gzip',
+            path: path.join(__dirname, 'logs')
+        })
+        : process.stdout
+}));
+app.use(cors());
+
+export const server = http.createServer(app);
+server.listen(port);
+
+server.on('error', onError)
+server.on('listening', onListening)
