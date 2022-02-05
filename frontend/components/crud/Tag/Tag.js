1c764765529987865e3287138f208fdfd6ead414
Elisha Bello <elishabello2014@gmail.com>
2022-02-06 00:08:57 +0100

    CRUD Operation On Tags

--- frontend/components/crud/Tag/Tag.js
+++ frontend/components/crud/Tag/Tag.js
@@ -2,42 +2,39 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import Router from "next/router";
 import { isAuth, getCookie } from "../../../actions/auth";
-import {
-    createCategory, getCategories
-} from "../../../actions/category";
+import { createTag, getTags, deleteTag} from "../../../actions/tag";
 import data from "bootstrap/js/src/dom/data";
 
-const Category = () => {
+const Tag = () => {
     const [values, setValues] = useState({
-        name: "",
+        name: '',
         error: false,
         success: false,
-        categories: [],
+        tags: [],
         removed: false,
         reload: false
     });
 
-    const { name, error, success, categories, removed, reload } = values;
+    const { name, error, success, tags, removed, reload } = values;
     const token = getCookie('token');
 
     useEffect(() => {
-        loadCategories();
-    }, [reload]);
+        loadTags();
+    }, [reload])
 
-    const loadCategories = () => {
-        getCategories().then(data => {
+    const loadTags = () => {
+        getTags().then(data => {
             if (data.error) {
-                console.log(data.error);
+                console.log(error)
             } else {
-                setValues({ ...values, categories: data })
+                setValues({ ...values, tags: data });
             }
-        });
+        })
     }
-
 
     const handleSubmit = e => {
         e.preventDefault();
-        createCategory({ name }, token).then(data => {
+        createTag({ name }, token).then(data => {
             if (data.error) {
                 setValues({ ...values, error: data.error, success: false });
             } else {
@@ -48,56 +45,73 @@ const Category = () => {
                     name: '',
                     removed: !removed,
                     reload: !reload
-                })
+                });
             }
-        })
+        });
     };
 
     const handleChange = e => {
         setValues({
             ...values,
-            name: e.target.name,
+            name: e.target.value,
             error: false,
             success: false,
             removed: ''
         });
-    }
+    };
 
     const deleteConfirm = slug => {
+        let answer = window.confirm('Are you sure you want to delete this tag?');
 
-    }
+        if (answer) {
+            deleteTag(slug, token).then(data => {
+                if (data.error) {
+                    setValues({ ...values, error: data.error, success: false });
+                } else {
+                    setValues({
+                        ...values,
+                        error: false,
+                        success: false,
+                        name: '',
+                        removed: !removed,
+                        reload: !reload
+                    });
+                }
+            });
+        }
+    };
 
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <div className='form-group'>
-                    <label htmlFor='inputName'>Category Name</label>
+                    <label htmlFor='inputName'>Tag Name</label>
                     <input
                         onChange={handleChange}
                         value={name}
                         type='text'
                         className='form-control'
                         id='inputName'
-                        placeholder='Enter category name'
+                        placeholder='Enter tag name'
                         required
                     />
                 </div>
                 <button type='submit' className='btn btn-primary'>
-                    Create Category
+                    Create Tag
                 </button>
             </form>
-            <div className='mt-4 mb-5'>
-                {categories.map(category => (
+            <div className='mt-4'>
+                {tags.map(tag => (
                     <button
-                        onDoubleClick={() => deleteConfirm(category.slug)}
+                        onDoubleClick={() => deleteConfirm(tag.slug)}
                         title='Double click to delete'
-                        key={category._id}
+                        key={tag._id}
                         type='button'
                         className='btn btn-outline-primary mr-1 mt-2'
                         data-toggle='modal'
                         data-target='#exampleModalCenter'
                     >
-                        {category.name}
+                        {tag.name}
                     </button>
                 ))}
             </div>
@@ -105,4 +119,4 @@ const Category = () => {
     );
 };
 
-export default Category;
+export default Tag
